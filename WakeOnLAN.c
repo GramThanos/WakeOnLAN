95cece480615469294d0560518a1b9e100585fb7
Luca Palano <luca.palano+git@lpzone.it>
2021-12-25 11:40:54 +0100

    [MAC OS] Fix the printf format entry in order to remove compilation warning

--- WakeOnLAN.c
+++ WakeOnLAN.c
@@ -111,7 +111,7 @@ int main(int argc, const char* argv[]){
 	}
 
 	// Print address
-	printf("Packet will be sent to %02hhx:%02x:%02x:%02x:%02x:%02x\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+	printf("Packet will be sent to %02x:%02x:%02x:%02x:%02x:%02x\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
 	// Check if a broadcast address was given too
 	if(argc > 2){
