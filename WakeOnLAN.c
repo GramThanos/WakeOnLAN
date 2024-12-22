0e823238fca7e406d7059685e1e45209056ec1c5
Luca Palano <1812530+lucapalano@users.noreply.github.com>
2024-12-22 12:37:18 +0100

    Add support for parsing MAC addresses to wake from file
    -f option has been introduced to allow to provide file with a list of MAC
    addresses to wake.

--- WakeOnLAN.c
+++ WakeOnLAN.c
@@ -1,5 +1,5 @@
 /*
- * WakeOnLAN v0.3
+ * WakeOnLAN v1.0
  * A simple C program that sends a magic packet
  *
  *
@@ -36,6 +36,7 @@
 	#include <arpa/inet.h>
 	#include <netdb.h>
 	#include <net/if.h>
+	#include <unistd.h>
 #endif
 #ifdef _WIN32
 	#include <winsock2.h>
@@ -49,6 +50,7 @@
 	#include <netinet/in.h>
 	#include <arpa/inet.h>
 	#include <net/if.h>
+	#include <unistd.h>
 #endif
 
 #include <stdio.h>
@@ -106,7 +108,7 @@ static int sendMagicPacket(const unsigned char *packet,
 
 	// Set broadcast
 	if (!packet) {
-		printf("Invalid packet address.\n");
+		printf("Invalid packet.\n");
 		return 1;
 	}
 
@@ -116,8 +118,6 @@ static int sendMagicPacket(const unsigned char *packet,
 	} else {
 		broadcastAddress = "255.255.255.255";
 	}
-
-	printf("Broadcast address %s will be used.\n", broadcastAddress);
 
 	// MacOS and Linux
 	#if defined(__APPLE__) || defined(__linux)
@@ -206,23 +206,90 @@ static int sendMagicPacket(const unsigned char *packet,
 	return 0;
 }
 
+/**
+ * @brief Read MAC addresses from a file and store them in an array
+ *
+ * @param filename Path to the file containing MAC addresses
+ * @param mac_addresses_list Pointer to array that will store the MAC addresses
+ * @param mac_addresses_count Pointer to store number of MAC addresses read
+ *
+ * @return 0 on success, 1 on failure
+ */
+static int getMacAddressesFromFile(const char *filename, unsigned int ***mac_addresses_list, int *mac_addresses_count) {
+	FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Could not open file %s\n", filename);
+        return 1;
+    }
+
+    char line[18] = {0}; // XX:XX:XX:XX:XX:XX + '\0' (18 chars)
+    *mac_addresses_count = 0;
+    *mac_addresses_list = NULL;
+
+    while (fgets(line, sizeof(line), file)) {
+        // Remove newline if present
+		if(strlen(line) < 17) {
+			continue;
+		}
+
+        unsigned int *mac = (unsigned int *)malloc(6 * sizeof(unsigned int));
+        if (mac == NULL) {
+            printf("Failed to allocate memory for MAC address\n");
+            fclose(file);
+            return 1;
+        }
+
+        int result = sscanf(line, "%x:%x:%x:%x:%x:%x",
+                            &mac[0], &mac[1], &mac[2],
+                            &mac[3], &mac[4], &mac[5]);
+        if (result != 6) {
+            printf("Invalid MAC address format in file: %s  (error %d)\n", filename, result);
+            free(mac);
+            continue;
+        }
+
+        // Reallocate array to hold new MAC address
+        unsigned int **new_list = realloc(*mac_addresses_list,
+                                         (*mac_addresses_count + 1) * sizeof(unsigned int *));
+        if (new_list == NULL) {
+            printf("Failed to reallocate memory for MAC addresses list\n");
+            free(mac);
+            fclose(file);
+            return 1;
+        }
+
+        *mac_addresses_list = new_list;
+        (*mac_addresses_list)[*mac_addresses_count] = mac;
+        (*mac_addresses_count)++;
+    }
+
+    fclose(file);
+
+    if (*mac_addresses_count == 0) {
+        printf("No valid MAC addresses found in file\n");
+        return 1;
+    }
+
+    return 0;
+}
+
 // Main Program
-int main(int argc, const char* argv[]){
-	// Default broadcast address
+int main(int argc, char* argv[]){
 	char broadcastAddress[IPV4_ADDR_LEN+1] = {0};
 
-	// Packet buffer
+	// Magic packet buffer
 	unsigned char packet[102];
 
-	// Mac address
-	unsigned int mac[6];
+	// Mac addresses list
+	unsigned int **mac_addresses_list = NULL;
+	int mac_addresses_count = 0;
 
 	// Help variables
-	int i, error = 0;
+	int i, error, opt, broadcast = 0;
 
-	// If no arguments
+	// If no arguments given, print usage
 	if(argc < 2){
-		printf("Usage:\n%s <mac address> [<broadcast address>]", argv[0]);
+		printf("Usage:\n%s [<mac address> | -f macs-list.txt] [<broadcast address>]", argv[0]);
 		#ifndef _WIN32
 			printf(" [<interface>]");
 		#endif
@@ -230,12 +297,50 @@ int main(int argc, const char* argv[]){
 		exit(EXIT_FAILURE);
 	}
 
-	// Parse Mac Address
-	i = sscanf(argv[1],"%x:%x:%x:%x:%x:%x", &(mac[0]), &(mac[1]), &(mac[2]), &(mac[3]), &(mac[4]), &(mac[5]));
-	if(i != 6){
-		printf("Invalid mac address. Please specify a valid mac address in the format xx:xx:xx:xx:xx:xx\n");
-		exit(EXIT_FAILURE);
-	}
+    // Parsing arguments using getopt
+    char *file_name = NULL;
+    while ((opt = getopt(argc, argv, "f:")) != -1) {
+        switch (opt) {
+            case 'f':
+                file_name = optarg;
+                break;
+        }
+    }
+
+    if (file_name) {
+        printf("Parsing file name: %s . Invalid MAC addresses will be discarded.\n", file_name);
+
+		error = getMacAddressesFromFile(file_name, &mac_addresses_list, &mac_addresses_count);
+		if (error)
+		{
+			printf("Failed to read MAC addresses from file.\n");
+			goto free_and_exit;
+		}
+    } else {
+		unsigned int *mac = (unsigned int *)malloc(sizeof(unsigned int) * 6);
+		if (mac == NULL) {
+			printf("Failed to allocate memory for mac address.\n");
+			exit(EXIT_FAILURE);
+		}
+
+		// Parse Mac Address
+		i = sscanf(argv[1],"%x:%x:%x:%x:%x:%x", &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]);
+		if(i != 6) {
+			printf("Invalid mac address. Please specify a valid mac address in the format xx:xx:xx:xx:xx:xx\n");
+			free(mac);
+			exit(EXIT_FAILURE);
+		}
+
+		mac_addresses_list = (unsigned int **)malloc(sizeof(unsigned int *));
+		if (mac_addresses_list == NULL) {
+			printf("Failed to allocate memory for mac address list.\n");
+			free(mac);
+			exit(EXIT_FAILURE);
+		}
+
+		mac_addresses_list[0] = mac;
+		mac_addresses_count = 1;
+    }
 
 	// Check if a broadcast address was given too
 	if(argc > 2){
@@ -243,20 +348,30 @@ int main(int argc, const char* argv[]){
 		i = sscanf(argv[2],"%d.%d.%d.%d", &i, &i, &i, &i);
 		if(i == 4){
 			strncpy(broadcastAddress, argv[2], IPV4_ADDR_LEN);
+			broadcast = 1;
 		}
 	}
 
-	// Create Magic Packet
-	createMagicPacket(packet, mac);
+	// Create Magic Packets and send them
+	for(i = 0; i < mac_addresses_count; i++) {
+		unsigned int *mac = mac_addresses_list[i];
 
-	// Send Magic Packet
-	error = sendMagicPacket(packet, NULL, argc > 3 ? argv[3] : NULL);
-	if(error) {
-		printf("Failed to send magic packet.\n");
-		exit(EXIT_FAILURE);
-	} else {
-		printf("Magic packet sent to %02x:%02x:%02x:%02x:%02x:%02x with broadcast %s\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], broadcastAddress);
+		createMagicPacket(packet, mac);
+
+		error = sendMagicPacket(packet, broadcast ? broadcastAddress : NULL, argc > 3 ? argv[3] : NULL);
+		if(error) {
+			printf("Failed to send magic packet.\n");
+			exit(EXIT_FAILURE);
+		} else {
+			printf("Magic packet sent to %02x:%02x:%02x:%02x:%02x:%02x...\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+		}
 	}
+
+free_and_exit:
+	for(i = 0; i < mac_addresses_count; i++) {
+		free(mac_addresses_list[i]);
+	}
+	free(mac_addresses_list);
 
 	exit(EXIT_SUCCESS);
 }
